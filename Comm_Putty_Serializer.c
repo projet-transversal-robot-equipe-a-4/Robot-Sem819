51b4b94cfe56a0425df5e90364c036849cca34aa
Guillaume Sachet <Guill.sac@gmail.com>
2019-04-04 10:32:50 +0200

    Guillaume sachet serializer courant (#3)
    * Add files via upload
    
    * Patch : tout marche sauf courant

--- Comm_Putty_Serializer.c
+++ Comm_Putty_Serializer.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 sfr16 RCAP4 = 0xE4;
-
+sfr16 RCAP2 = 0xCA;
 bit epreuve = 0;
 char vitesse1[8]= "20";
 char vitesse2[8]= "20";
@@ -20,6 +20,9 @@ sbit TRIG = P1^7;
 unsigned int temps_ecoule;
 int distance;
 
+sfr16 ADC0 = 0xBE;
+float Mesure_courant;
+float Courant;
 
 void init(){
 	WDTCN = 0xDE;   // Dévalidation du watchdog 
@@ -34,6 +37,21 @@ void init(){
 	P1MDOUT |= 0xC0; //P1.6 et P1.7 en sortie
 	
 	out_servo = 0;
+	
+	REF0CN |= 0x03; // enable Vref	
+}
+
+void Config_ADC(){
+	AMX0CF = 0x00; // Configuration register AIN0 et AIN1 independantes + AIN6 et AIN7 inde
+	AMX0SL = 0x00; // select AIN0
+	ADC0CF = 0x80; // gain de 1 et config horloge SAR avec oscillo externe
+	AD0TM = 0; // track ing continous
+	AD0CM0 = 0; // start conversion mode select
+	AD0CM1 = 0; // start conversion mode select
+	AD0LJST = 0; // left justified
+	AD0EN = 1; // enable ADC0
+	AD0INT = 0; // flag set to 0
+	AD0WINT = 0; //   ADC 0 WINDOW COMPARE INTERRUPT FLAG
 }
 
 void oscillo(){
@@ -55,7 +73,7 @@ void config_UART1(){
 	SCON1 |= 0x50;
 }	
 
-void config_Timers(){
+void config_Timer_1(){
 	CKCON|=0x10;
 	PCON |= 0x90;
 	TH1 = 184;
@@ -67,6 +85,10 @@ void config_Timers(){
 void config_Comparator(){
 	CPT0CN = 0x80;
 }
+void config_Timer_2(){
+	CKCON &= ~0x20;
+	TR2 = 1;
+}
 void config_Timer_3(){
 	TMR3CN = 0x02;
 }
@@ -74,28 +96,18 @@ void config_Timer_4()
 {
 	T4CON &= ~0xC3;
 	RCAP4 = 62771; //De base position au centre (0°)
-	TL4 = RCAP4L;
-	TH4 = RCAP4H;
 	T4CON |= 0x04;
 }
 
-void Delay(unsigned int millis)
+void Delay_milli(unsigned int millis)
 {
     unsigned int i;
-    CKCON &= ~0x40;
-    T4CON = 0;
-    RCAP4 = (65536 - (22118400 / (12 * 1000)));
-    TL4 = RCAP4L;
-    TH4 = RCAP4H;
-    
-    T4CON |= 0x04;
-    
+    RCAP2 = (65536 - (22118400 / (12 * 1000)));
+		TF2 = 0;
     for (i = 0; i < millis; i++)
     {
-        while((T4CON & 0x80) == 0)
-        {
-            T4CON &= ~0x80;
-        }
+        while(TF2 == 0){}
+				TF2 = 0;
     }
 }
 
@@ -118,17 +130,25 @@ void Delay_Micro(unsigned int micros)
         }
     }
 }
+
+void AnalogEntree(){
+	AMX0SL = 0x00; // select AIN0
+	AD0BUSY = 1; // start conversion
+	while (AD0BUSY == 1) {}
+	Courant = Mesure_courant;
+}
+
 void Putchar0(char c)
 {
 	SBUF0 = c;
-	while(TI0 == 0){};
+	while(TI0 == 0){}
 	TI0 = 0;
 }
 
 void Putchar1(char c)
 {
 	SBUF1 = c;
-	while((SCON1 & 0x02) == 0){}; //Flag passe à 1 à la fin de transmission
+	while((SCON1 & 0x02) == 0){} //Flag passe à 1 à la fin de transmission
 	SCON1 &= ~0x02; //Flag de transmission Mis à 0
 }
 
@@ -256,79 +276,76 @@ void Encodage_uart0(){
 				deplacement(vitesse1_tmp,vitesse2_tmp);
 			}
 		}
-		else if (strcmp(str_putty, "RD\r") == 0 && epreuve){
+		else if (strcmp(str_putty,"RD\r") == 0 && epreuve){
 			strcpy(vitesse1_tmp,"-15");
 			strcpy(vitesse2_tmp,"15");
 			deplacement(vitesse1_tmp,vitesse2_tmp);
-			Delay(600);
+			Delay_milli(745);
 			send_string1("stop\r");
-	}
-	else if (strcmp(str_putty, "RG\r") == 0 && epreuve){
-			strcpy(vitesse1_tmp,"15");
-			strcpy(vitesse2_tmp,"-15");
-			deplacement(vitesse1_tmp,vitesse2_tmp);
-			Delay(600);
-			send_string1("stop\r");
-	}
-	else if (strcmp(str_putty, "RCG\r") == 0 && epreuve){
-			strcpy(vitesse1_tmp,"15");
-			strcpy(vitesse2_tmp,"-15");
-			deplacement(vitesse1_tmp,vitesse2_tmp);
-			Delay(1200);
-			send_string1("stop\r");
-	}
-	else if (strcmp(str_putty, "RCD\r") == 0 && epreuve){
-			strcpy(vitesse1_tmp,"-15");
-			strcpy(vitesse2_tmp,"15");
-			deplacement(vitesse1_tmp,vitesse2_tmp);
-			Delay(1200);
-			send_string1("stop\r");
-	}
-	
-	else if (str_putty[0] == 'R' && str_putty[1] == 'A' && epreuve){
-			sscanf(str_putty, "%s %c:%s",commande,sens_rotation,commande_valeur);
-			test_valeur = atoi(commande_valeur);
-			if (sens_rotation == 'G') {
+		}
+		else if (strcmp(str_putty, "RG\r") == 0 && epreuve){
 				strcpy(vitesse1_tmp,"15");
 				strcpy(vitesse2_tmp,"-15");
-			} else if (sens_rotation == 'D') {
+				deplacement(vitesse1_tmp,vitesse2_tmp);
+				Delay_milli(745);
+				send_string1("stop\r");
+		}
+		else if (strcmp(str_putty, "RCG\r") == 0 && epreuve){
+				strcpy(vitesse1_tmp,"15");
+				strcpy(vitesse2_tmp,"-15");
+				deplacement(vitesse1_tmp,vitesse2_tmp);
+				Delay_milli(1350);
+				send_string1("stop\r");
+		}
+		else if (strcmp(str_putty, "RCD\r") == 0 && epreuve){
 				strcpy(vitesse1_tmp,"-15");
 				strcpy(vitesse2_tmp,"15");
-			} else {
-				send_string0("\r\n#\r\n>");
-			}
-			deplacement(vitesse1_tmp,vitesse2_tmp);
-			Delay(test_valeur*5);
-			send_string1("stop\r");
-			}
-	else if (str_putty[0] == 'R' && epreuve){
-			sscanf(str_putty, "%s %s",commande,commande_valeur);
-			test_valeur = atoi(commande_valeur);
-			strcpy(vitesse1_tmp,"15");
-			strcpy(vitesse2_tmp,"-15");
-			deplacement(vitesse1_tmp,vitesse2_tmp);
-			Delay(test_valeur);
-			send_string1("stop\r");
-			}
+				deplacement(vitesse1_tmp,vitesse2_tmp);
+				Delay_milli(1350);
+				send_string1("stop\r");
+		}
+		else if (str_putty[0] == 'R' && str_putty[1] == 'A' && epreuve){
+				sscanf(str_putty, "%s %c:%s",commande,sens_rotation,commande_valeur);
+				test_valeur = atoi(commande_valeur);
+				if (sens_rotation == 'G') {
+					strcpy(vitesse1_tmp,"15");
+					strcpy(vitesse2_tmp,"-15");
+				} else if (sens_rotation == 'D') {
+					strcpy(vitesse1_tmp,"-15");
+					strcpy(vitesse2_tmp,"15");
+				} else {
+					send_string0("\r\n#\r\n>");
+				}
+				deplacement(vitesse1_tmp,vitesse2_tmp);
+				Delay_milli(test_valeur*8);
+				send_string1("stop\r");
+				}
 			
 	// servomoteur 
-	else if (str_putty[0] == 'C' && str_putty[1] == 'S' && epreuve){
-				sscanf(str_putty, "%s %s %s : %s",commande,type,angle, commande_valeur);
-				if(strstr(commande_valeur, "-")) {
-					sscanf(commande_valeur,"-%s",commande_valeur);
-					test_valeur = - atoi(commande_valeur);
-				} else {
-					test_valeur = atoi(commande_valeur);
-				}
-				servomoteur_H(test_valeur);
-				send_string0("AS H\r\n>");
-				}
+		else if (str_putty[0] == 'C' && str_putty[1] == 'S' && epreuve){
+					sscanf(str_putty, "%s %s %s : %s",commande,type,angle, commande_valeur);
+					if(strstr(commande_valeur, "-")) {
+						sscanf(commande_valeur,"-%s",commande_valeur);
+						test_valeur = - atoi(commande_valeur);
+					} else {
+						test_valeur = atoi(commande_valeur);
+					}
+					servomoteur_H(test_valeur);
+					send_string0("\r\nAS H\r\n>");
+		}
 	
 	// telemetre
-	else if (str_putty[0] == 'M' && str_putty[1] == 'O' & str_putty[2] == 'U' && epreuve){
-		trig();
-	}
-		
+		else if (str_putty[0] == 'M' && str_putty[1] == 'O' & str_putty[2] == 'U' && epreuve){
+			trig();
+		}
+	// mesure courant
+		else if (str_putty[0] == 'M' && str_putty[1] == 'I' && epreuve){
+			AnalogEntree();
+			sprintf(commande_valeur,"%.3f",Courant);
+			send_string0("Courant instantane : ");
+			send_string0(commande_valeur);
+			send_string0("\r\n>");
+		}
 			
 	else {
 			send_string1(str_putty);
@@ -336,6 +353,7 @@ void Encodage_uart0(){
 	str_putty = "";
 	}
 }
+
 void Encodage_uart1(){
 	char * str_putty;
 	int cur_len;
@@ -393,7 +411,7 @@ void interrupt_reception1(void) interrupt 20{
 		//test si reception est fini
 		Encodage_uart1();
 		
-	}	
+	}
 }
 void ISR_TIMER4(void) interrupt 16 //Routine interruption overflow timer 4
 { 
@@ -409,23 +427,26 @@ void ISR_TIMER4(void) interrupt 16 //Routine interruption overflow timer 4
 		condition_timer4 = 0;
 		out_servo = 0;
 	}
-	
-	TL4 = RCAP4L;
-	TH4 = RCAP4H;
 	T4CON &=~0x80;
+	TL4 = RCAP4L;
+  TH4 = RCAP4H;
 }
+
+void finConversion() interrupt 15 {
+	AD0INT = 0; // flag set 0
+	Mesure_courant = (ADC0/4096.0)*10.192;
+}
+
 void main(){	
 	init();
 	oscillo();
-	config_Timers();
+	config_Timer_1();
+	config_Timer_2();
 	config_Timer_3();
 	config_Timer_4();
 	config_Comparator();
 	config_UART0();
 	config_UART1();
-
-	while(1)
-	{
-		
-	}
+	Config_ADC();
+	while(1){}
 }
